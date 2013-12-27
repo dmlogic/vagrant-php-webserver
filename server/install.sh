acf956091ecc60a1f1238d2604a78af476f96848
Darren Miller <darren@dmlogic.com>
2013-12-27 22:07:12 +0000

    Mac apache setup

--- server/install.sh
+++ server/install.sh
@@ -13,11 +13,11 @@ echo ">>> Installing Base Items"
 # Install base items
 sudo apt-get install -y vim curl wget build-essential python-software-properties mysql-server
 
-    # echo ">>> Installing latest, greatest PHP"
-    # sudo add-apt-repository -y ppa:ondrej/php5 # php5.5
+    echo ">>> Installing latest, greatest PHP"
+    sudo add-apt-repository -y ppa:ondrej/php5 # php5.5
 
-     echo ">>> Installing PHP5.4"
-    sudo add-apt-repository -y ppa:ondrej/php5-oldstable # php5.4
+    #echo ">>> Installing PHP5.4"
+    #sudo add-apt-repository -y ppa:ondrej/php5-oldstable # php5.4
 
 # Update Again
 sudo apt-get update
@@ -26,22 +26,22 @@ sudo apt-get update
 sudo apt-get install -y php5 php5-mysql php5-curl php5-gd php5-mcrypt
 
     # apache
-    # echo ">>> Installing apache2"
-    # sudo apt-get install -y apache2 libapache2-mod-php5
-    # sudo a2enmod rewrite
-    # sudo cp /vagrant/server/apache2.conf /etc/apache2/sites-available/000-default.conf
-    # inifolder=/etc/php5/apache2
+    echo ">>> Installing apache2"
+    sudo apt-get install -y apache2 libapache2-mod-php5
+    sudo a2enmod rewrite
+    sudo cp /vagrant/server/apache2.conf /etc/apache2/sites-available/000-default.conf
+    inifolder=/etc/php5/apache2
 
     # nginx
-    echo ">>> Installing nginx"
-    sudo service apache2 stop
-    sudo apt-get purge apache2
-    sudo apt-get install -y nginx php5-fpm
-    sudo service nginx start
-    sudo cp /vagrant/server/nginx.conf /etc/nginx/sites-available/default
-    inifolder=/etc/php5/fpm
+    # echo ">>> Installing nginx"
+    # sudo service apache2 stop
+    # sudo apt-get purge apache2
+    # sudo apt-get install -y nginx php5-fpm
+    # sudo service nginx start
+    # sudo cp /vagrant/server/nginx.conf /etc/nginx/sites-available/default
+    # inifolder=/etc/php5/fpm
 
-    echo $inifolder
+    # echo $inifolder
 
 # PHP Config
 echo ">>> Configuring PHP"
@@ -56,11 +56,11 @@ sudo sed -i "s/bind-address.*/#bind-address        = 127.0.0.1/" /etc/mysql/my.c
 mysql -u root -ppassword < /vagrant/server/database.sql
 sudo service mysql restart
 
-#sudo service apache2 restart
-sudo service nginx restart
+sudo service apache2 restart
+# sudo service nginx restart
 
 # Composer and GIT only needed for a "real" server
-# echo ">>> Installing GIT and Composer"
-# sudo apt-get install git
-# curl -sS https://getcomposer.org/installer | php
-# sudo mv composer.phar /usr/local/bin/composer
+echo ">>> Installing GIT and Composer"
+sudo apt-get install git
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
